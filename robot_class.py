f65ae54e23d334aead6ed68689ab92030f49abc7
sandeeppaulraj <sandeeppaulraj@gmail.com>
2018-06-07 01:19:17 -0700

    update robot class

--- robot_class.py
+++ robot_class.py
@@ -91,20 +91,19 @@ class robot:
         ##    as list.append([index, dx, dy]), this format is important for data creation done later
         
         ## TODO: return the final, complete list of measurements
+        for i in range(self.num_landmarks):
+            dx = self.landmarks[i][0] - self.x + self.rand() * self.measurement_noise
+            dy = self.landmarks[i][1] - self.y + self.rand() * self.measurement_noise    
+            if self.measurement_range < 0.0 or abs(dx) + abs(dy) <= self.measurement_range:
+                measurements.append([i, dx, dy])
+
         return measurements
 
-
-    # --------
-    # make_landmarks:
-    # make random landmarks located in the world
-    #
     def make_landmarks(self, num_landmarks):
         self.landmarks = []
         for i in range(num_landmarks):
-            self.landmarks.append([round(random.random() * self.world_size),
-                                   round(random.random() * self.world_size)])
+            self.landmarks.append([round(random.random() * self.world_size), round(random.random() * self.world_size)])
         self.num_landmarks = num_landmarks
-
 
     # called when print(robot) is called; prints the robot's location
     def __repr__(self):
